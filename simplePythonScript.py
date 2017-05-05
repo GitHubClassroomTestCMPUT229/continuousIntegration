6c4654c10e3f34f5c7a95cbac08ff0da7adcb8ac
Stuart Hoye <hoye@cs.ualberta.ca>
2017-05-05 12:08:13 -0600

    Try accessing the results folder from within Travis.

--- simplePythonScript.py
+++ simplePythonScript.py
@@ -1,8 +1,19 @@
 import spimgrader
 import subprocess
+import os
 
 def main():
+    # Call spimgrader to apply files to student submissions
     spimgrader.main()
+    # After call, results are in ./results
+    # Do whatever we need to with the results files
+    # eg: Compare them with expected results
+    # eg: Send the results to the student
+    # eg: Send the results to the teaching team
+    files = os.listdir("./results")
+    for f in files:
+        f = open("./results/" + f, "r")
+        print(f.read())
 
 if __name__ == "__main__":
     main()
