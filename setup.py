95f2710cf624445ce67b684f3c20208855ec06de
Stuart Hoye <hoye@cs.ualberta.ca>
2017-05-05 10:24:50 -0600

    Let the script read from subdirectories

--- setup.py
+++ setup.py
@@ -3,9 +3,14 @@ import subprocess
 # Use subprocess to invoke the Make process for SPIM.  If successful, we will have spim 
 # installed remotely in the Travis build environment each time the student submits their
 # code.
+
+# Resource for altering working directory within Python:
+# http://stackoverflow.com/questions/21406887/subprocess-changing-directory
+# User: glglgl
 def main():
-    subprocess.call(["./spim-8.0/spim/make", "spim"])
-    subprocess.call(["./spim-8.0/spim/make", "install"])
+    subprocess.call("test.sh", cwd="./spim/spim/")
+    # subprocess.call(["./spim/spim/make", "spim"], cwd="./spim/spim/")
+    # subprocess.call(["./spim/spim/make", "install"], cwd="./spim/spim/")
 
 if __name__ == "__main__":
     main()
